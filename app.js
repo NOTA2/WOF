eda746a084aa39475ee88d409ac6b4d2c4ac71c8
skaska121212@gmail.com <skaska121212@gmail.com>
2018-08-20 16:21:33 +0900

    appjs 수정

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const PORT = 3000;
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
 var conn = require('./config/db')();
 var app = require('./config/express')();
@@ -8,16 +10,12 @@ var auth = require('./routes/auth')(passport);
 app.use('/auth/', auth);
 
 
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
 io.on('connection', (socket) => {
   console.log('connection');
 
 
-  socket.on('abc', (data) => {
-    console.log('hello');
-    console.log(data);
-     socket.emit('abc', '병신드류ㅠ');
+  socket.on('testBeep', (data) => {
+     socket.emit('testBeep', 'OK');
   })
 
 
